dd5a0860d673a9009d9816353b197fe41b68abf3
jagarcell <jagarcell@gmail.com>
2026-03-04 17:31:50 -0500

    create users request validation

---
+++ app/Http/Requests/Api/UsersIndexRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UsersIndexRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, array<int, string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'search' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation failed.',
+            'data' => null,
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
